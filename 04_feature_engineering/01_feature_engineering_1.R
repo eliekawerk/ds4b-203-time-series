f23528ce843bfdec1f9d77663f50adccd644ea8a
Bryan Clark <bryan.j.clark08@gmail.com>
2020-09-25 14:27:04 -0400

    advanced feature engineering

--- 04_feature_engineering/01_feature_engineering_1.R
+++ 04_feature_engineering/01_feature_engineering_1.R
@@ -147,48 +147,152 @@ data_prep_signature_tbl %>%
 # - tk_augment_fourier
 
 # Data Prep
+data_prep_signature_tbl %>%
+    plot_acf_diagnostics(optin_time, optins_trans)
 
+data_prep_fourier_tbl <- data_prep_signature_tbl %>%
+    tk_augment_fourier(optin_time, .periods = c(7, 14, 30, 90, 365), .K = 2)
+
+data_prep_fourier_tbl %>% glimpse()
 
 # Model
-
+model_formula_fourier <- as.formula(
+    optins_trans ~ splines::ns(index.num, knots = quantile(index.num, probs = c(0.25, 0.5))) +
+        . +
+        (as.factor(week2) * wday.lbl)
+)
 
 # Visualize
+data_prep_fourier_tbl %>%
+    plot_time_series_regression(
+        optin_time,
+        .formula = model_formula_fourier,
+        .show_summary = T
+    )
+
 
 
 # 4.0 LAGS ----
 # - tk_augment_lags()
 
 # Data Prep
+data_prep_fourier_tbl %>%
+    plot_acf_diagnostics(optin_time, optins_trans, .lags = 56:600)
+
+
+data_prep_lags_tbl <- data_prep_fourier_tbl %>%
+    tk_augment_lags(optins_trans, .lags = c(57, 63, 70)) %>%
+    drop_na()
 
 
 # Model
-
+model_formula <- as.formula(
+    optins_trans ~ splines::ns(index.num, knots = quantile(index.num, probs = c(0.25, 0.4))) +
+        . +
+        (as.factor(week2) * wday.lbl)
+)
 
 # Visualize
+data_prep_lags_tbl %>%
+    plot_time_series_regression(
+        optin_time,
+        .formula = model_formula,
+        .show_summary = T
+    )
 
+data_prep_fourier_tbl %>%
+    filter_by_time(.start_date = "2018-09-13") %>%
+    plot_time_series_regression(
+        optin_time,
+        .formula = model_formula,
+        .show_summary = T
+    )
 
 # 5.0 SPECIAL EVENTS ----
 
 # Data Prep
+learning_labs_daily_tbl <- learning_labs_tbl %>%
+    mutate(event_date = ymd_hms(event_date)) %>%
+    summarise_by_time(event_date, "day", event = n())
 
+# better than lags version
+data_prep_events_tbl <- data_prep_fourier_tbl %>%
+    left_join(
+        learning_labs_daily_tbl,
+        by = c("optin_time" = "event_date")
+    ) %>%
+    replace_na(list(event = 0))
+    
+data_prep_events_tbl %>% glimpse()
+
+g <- data_prep_events_tbl %>% 
+    plot_time_series(optin_time, optins_trans, .interactive = F) +
+    geom_point(color = "red", data = . %>% filter(event == 1))
+
+ggplotly(g)
 
 # Model
-
+model_formula
 
 # Visualize
+data_prep_events_tbl %>%
+    plot_time_series_regression(
+        optin_time,
+        .formula = model_formula,
+        .show_summary = T
+    )
 
 # 6.0 EXTERNAL LAGGED REGRESSORS ----
 # - xregs
 
 # Data Prep
+ga_prep_tbl <- google_analytics_summary_tbl %>%
+    mutate(date = ymd_h(dateHour)) %>%
+    summarize_by_time(date, .by = "day", across(pageViews:sessions, .fns = sum)) %>%
+    mutate(across(pageViews:sessions, .fns = log1p)) %>%
+    mutate(across(pageViews:sessions, .fns = standardize_vec))
 
+ga_prep_tbl
+
+data_prep_ga_tbl <- data_prep_events_tbl %>%
+    left_join(
+        ga_prep_tbl,
+        by = c("optin_time" = "date")
+    ) %>%
+    drop_na()
+
+data_prep_ga_tbl %>% glimpse()
+
+data_prep_ga_tbl %>%
+    plot_acf_diagnostics(
+        optin_time,
+        optins_trans,
+        .ccf_vars = pageViews:sessions,
+        .show_ccf_vars_only = T
+    )
 
 # Model
-
+model_formula <- as.formula(
+    optins_trans ~ splines::ns(index.num, knots = quantile(index.num, probs = c(0.25, 0.4))) +
+        . +
+        (as.factor(week2) * wday.lbl)
+)
 
 # Visualize
+data_prep_ga_tbl %>%
+    plot_time_series_regression(
+        optin_time,
+        .formula = model_formula,
+        .show_summary = T
+    )
 
 # 7.0 RECOMMENDATION ----
 # - Best model: 
 # - Best Model Formula:
 
+# linear regression model
+model_fit_best_lm <- lm(model_formula, data = data_prep_events_tbl)
+
+model_fit_best_lm$terms %>% formula()
+
+write_rds(model_fit_best_lm, "00_models/model_fit_best_lm.rds")
---
+++ 04_feature_engineering/02_advanced_feature_engineering.R
@@ -0,0 +1,166 @@
+# BUSINESS SCIENCE UNIVERSITY
+# DS4B 203-R: TIME SERIES FORECASTING FOR BUSINESS
+# MODULE: ADVANCED FEATURE ENGINEERING
+
+# GOALS ----
+# - LEARN ADVANCED FEATURE ENGINEERING TECHNIQUES & WORKFLOW
+
+# OBJECTIVES ----
+# - IMPLEMENT RECIPES PIPELINE
+# - APPLY MODELTIME WORKFLOW & VISUALIZE RESULTS
+# - COMPARE SPLINE MODEL VS LAG MODEL
+
+# LIBRARIES & DATA ----
+
+# Time Series ML
+library(tidymodels)
+library(modeltime)
+
+# Core
+library(tidyverse)
+library(lubridate)
+library(timetk)
+
+# Data
+subscribers_tbl   <- read_rds("00_data/mailchimp_users.rds")
+learning_labs_tbl <- read_rds("00_data/learning_labs.rds") 
+
+# Data Wrangle
+subscribers_prep_tbl <- subscribers_tbl %>%
+    summarise_by_time(optin_time, .by = "day", optins = n()) %>%
+    pad_by_time(.pad_value = 0)
+subscribers_prep_tbl
+
+learning_labs_prep_tbl <- learning_labs_tbl %>%
+    mutate(event_date = ymd_hms(event_date)) %>%
+    summarise_by_time(event_date, .by = "day", event = n())
+learning_labs_prep_tbl
+
+# Data Transformation
+subscribers_transformed_tbl <- subscribers_prep_tbl %>%
+    
+    # Preprocess Target
+    mutate(optins_trans = log_interval_vec(optins, limit_lower = 0, offset = 1)) %>%
+    mutate(optins_trans = standardize_vec(optins_trans)) %>%
+    select(-optins) %>%
+    
+    # Fix missing values at beginning of series
+    filter_by_time(.start_date = "2018-07-03") %>%
+    
+    # Cleaning
+    mutate(optins_trans_cleaned = ts_clean_vec(optins_trans, period = 7)) %>%
+    mutate(optins_trans = ifelse(optin_time %>% between_time("2018-11-18", "2018-11-20"), 
+                                 optins_trans_cleaned,
+                                 optins_trans)) %>%
+    select(-optins_trans_cleaned)
+
+subscribers_transformed_tbl
+
+# Save Key Params
+limit_lower <- 0
+limit_upper <- 3650.8
+offset      <- 1
+std_mean    <- -5.25529020756467
+std_sd      <- 1.1109817111334
+
+# 1.0 STEP 1 - CREATE FULL DATA SET ----
+# - Extend to Future Window
+# - Add any lags to full dataset
+# - Add any external regressors to full dataset
+
+horizon <- 8 * 7
+lag_period <- 8 * 7
+rolling_periods <- c(30, 60, 90)
+
+data_prepared_full_tbl <- subscribers_transformed_tbl %>%
+    
+    # add future window
+    bind_rows(
+        future_frame(.data = ., .date_var = optin_time, .length_out = horizon)
+    ) %>%
+    
+    # autocorrelated lags
+    tk_augment_lags(optins_trans, .lags = lag_period) %>%
+    
+    # add rolling features
+    tk_augment_slidify(
+        .value   = optins_trans_lag56,
+        .f       = mean,
+        .period  = rolling_periods,
+        .align   = "center",
+        .partial = TRUE
+    ) %>%
+    
+    # add events
+    left_join(
+        learning_labs_prep_tbl,
+        by = c("optin_time" = "event_date")
+    ) %>%
+    replace_na(list(event = 0)) %>%
+    
+    # format columns
+    rename(lab_event = event) %>%
+    rename_with(.cols = contains("lag"), .fn = ~ str_c("lag_", .))
+
+data_prepared_full_tbl %>%
+    pivot_longer(-optin_time) %>%
+    plot_time_series(optin_time, value, name, .smooth = F)
+
+
+
+# 2.0 STEP 2 - SEPARATE INTO MODELING & FORECAST DATA ----
+data_prepared_tbl <- data_prepared_full_tbl %>%
+    filter(!is.na(optins_trans))
+
+forecast_tbl <- data_prepared_full_tbl %>%
+    filter(is.na(optins_trans))
+
+# 3.0 TRAIN/TEST (MODEL DATASET) ----
+
+
+
+# 4.0 RECIPES ----
+# - Time Series Signature - Adds bulk time-based features
+# - Spline Transformation to index.num
+# - Interaction: wday.lbl:week2
+# - Fourier Features
+
+
+
+# 5.0 SPLINE MODEL ----
+
+# * LM Model Spec ----
+
+
+# * Spline Recipe Spec ----
+
+
+# * Spline Workflow  ----
+
+
+# 6.0 MODELTIME  ----
+
+
+
+# 7.0 LAG MODEL ----
+
+# * Lag Recipe ----
+
+
+# * Lag Workflow ----
+
+
+# * Compare with Modeltime -----
+
+
+
+# 8.0 FUTURE FORECAST ----
+
+
+
+# 9.0 SAVE ARTIFACTS ----
+
+
+
+
+
